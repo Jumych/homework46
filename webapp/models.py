a906c379b5c4420837b53e3b18a5bf166e22d56f
Malikov_Jumadil <maleevish@gmail.com>
2022-11-09 19:35:22 +0600

    В модель Задачи добавил описание задачи

--- webapp/models.py
+++ webapp/models.py
@@ -5,8 +5,8 @@ STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'),
 class Task(models.Model):
     title = models.CharField(max_length=60, verbose_name='Название')
     status = models.CharField(max_length=25, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0], verbose_name='Статус')
-    description = models.TextField(max_length=60, verbose_name='Подробное описание')
     deadline = models.DateTimeField(null=True, blank=True, verbose_name='Дата выполнеия')
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='mОписание')
 
 
     def __str__(self):
